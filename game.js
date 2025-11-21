684cca1585031e1a315f40e570c71399e810b553
Tobiaha <122260640+Tobiaha@users.noreply.github.com>
2025-11-21 12:09:17 +0200

    computerChoice Function

--- game.js
+++ game.js
@@ -1,1 +1,21 @@
-'use strict';
+"use strict";
+
+// 1 rock 2 paper 3 scissors
+
+function computerChoice(result) {
+  result = Math.floor(Math.random() * 3) + 1;
+  console.log(result);
+  if (result === 1) {
+    result = "Rock";
+  } else if (result === 2) {
+    result = "Paper";
+  } else if (result === 3) {
+    result = "Scissors";
+  } else {
+    console.log("Error");
+  }
+  console.log(result);
+  return result;
+}
+
+const computerTurn = computerChoice();
